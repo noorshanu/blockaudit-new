9ccce4c7f350750f67c0a6adb1c6db35c0678f3f
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-04-11 22:08:32 +0530

    new

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,78 +1,79 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
 
-import Logo from '../assets/logo.png'
-import { NavLink } from 'react-router-dom'
+import Logo from "../assets/logo.png";
+
 function Footer() {
-  
   return (
-    <div className='container-wrapper'>
-      <div className='border-top-grad'>
+    <div className="container-wrapper">
+      <div className="border-hrefp-grad"></div>
 
-      </div>
-
-      <div className='footer mt-40px'>
+      <div className="footer">
         <div>
-          <div className='lodo-section'>
-            <img src={Logo} alt=''/>
+          <div className="lodo-section">
+            <img src={Logo} alt="" />
             <p>Secure your web3 future with BlockAudit's expert solutions.</p>
             <h2>contact@blockaudit.report</h2>
           </div>
         </div>
 
         <div>
-          <div className='lodo-section'>
+          <div className="lodo-section">
             <h2>Social Media</h2>
-
             <div>
-              <p>
-                Twitter 
-              </p>
+              <p>Twitter</p>
+              <p>LinkedIn</p>
+              <p>Telegram</p>
+              <p>Github</p>
+              <p>Medium</p>
+              
             </div>
-        
           </div>
         </div>
         <div>
-          <div className='lodo-section'>
+          <div className="lodo-section">
             <h2>Services</h2>
             <div>
-              <p>
-                Twitter 
-              </p>
+              <p>Audits</p>
+              <p>Pen Testing</p>
+              <p>Security Policies</p>
+              <p>Consulting</p>
+              <p>Risk Management</p>
             </div>
           </div>
         </div>
 
         <div>
-          <div className='lodo-section'>
+          <div className="lodo-section">
             <h2>Other Links</h2>
-            <div>
-              <p>
-                Twitter 
+
+            <div className="">
+              <p href="/" className="">
+                Privacy Policy
               </p>
+              <p href="/" className="">
+                Disclaimer{" "}
+              </p>
+              <p href="/terms-condition" className="">
+                T & C{" "}
+              </p>
+              <a href="/" className="l">
+                Cookies Policy
+              </a>
             </div>
           </div>
         </div>
-
       </div>
-      <div className='div-block-289'>
-        <div className='div-block-290'>
-        <div className="text-block-113">Secured by :  BlockAudit🔒</div>
+      <div className="div-block-289">
+        <div className="div-block-290">
+          <div className="text-block-113">Secured by : BlockAudit🔒</div>
         </div>
         <div className="text-block-114">
-        Copyright © 2022 | All Right Reserved
-                        </div>
-                      
-                        <div className="div-block-342">
-                            <NavLink  to="/privacy-policy"  className="link-9" >Privacy Policy</NavLink>
-                            <NavLink to="/cookies"  className="link-9 _2" >Cookies Policy</NavLink>
-                            <NavLink to="/disclaimer" className="link-9 _2" >Disclaimer </NavLink>
-                            <NavLink to="/terms-condition" className="link-9 _3" >Terms and Conditions </NavLink>
-                        </div>
+          Copyright © 2022 | All Right Reserved
         </div>
-        
-        </div>
-  )
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
