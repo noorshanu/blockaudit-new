03d7d928c514b76218f6d756ba2522352da784d9
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-04-12 15:57:04 +0530

    new

--- src/layouts/About.js
+++ src/layouts/About.js
@@ -1,79 +1,86 @@
 import React from "react";
-import './About.css'
-import Main1 from '../assets/about/main1.png'
-import Main2 from '../assets/about/main2.png'
-import Main3 from '../assets/about/main3.png'
-import Main4 from '../assets/about/main4.png'
-
-
+import "./About.css";
+import Main1 from "../assets/about/main1.png";
+import Main2 from "../assets/about/main2.png";
+import Main3 from "../assets/about/main3.png";
+import Main4 from "../assets/about/main4.png";
 
 function About() {
   return (
-   
+    <main>
+      <section>
+        <div className="container-wrapper">
+          <div className="text-center why-head">
+            <h1>What We Offer</h1>
+            <p>
+              We specialize in providing a range of services tailored to web3
+              projects. At BlockAudit, our dedicated team delivers expert
+              support and customized solutions to ensure your project thrives in
+              the ever-changing digital landscape.
+            </p>
+          </div>
 
-
-      <main>
-        <section>
-          <div className="container-wrapper">
-            <div className='text-center why-head'>
-              <h1>What We Offer</h1>
-              <p>We specialize in providing a range of services tailored to web3 projects. At BlockAudit, our dedicated team delivers expert support and customized solutions to ensure your project thrives in the ever-changing digital landscape.</p>
-         
+          <div className="grid-about">
+            <div className="boxes">
+              <img src={Main1} alt="" />
+              <div className="content">
+                <p>Smart Contract</p>
+                <h1>AUDITS</h1>
+                <p>
+                  Trust in our expertise to ensure optimal security and
+                  performance for your smart contracts within the dynamic web3
+                  landscape.
+                </p>
+                <button>Explore</button>
+              </div>
             </div>
 
-            <div className="grid-about">
-
-              <div className="boxes">
-                <img src={Main1} alt=""/>
-                <div className="content">
-                  <p>Smart Contract</p>
-                  <h1>AUDITS</h1>
-                  <p>Trust in our expertise to ensure optimal security and performance for your smart contracts within the dynamic web3 landscape.</p>
-                  <button>Explore</button>
-
-                </div>
+            <div className="boxes">
+              <img src={Main2} alt="" />
+              <div className="content">
+                <p>Smart Contract</p>
+                <h1>AUDITS</h1>
+                <p>
+                  Trust in our expertise to ensure optimal security and
+                  performance for your smart contracts within the dynamic web3
+                  landscape.
+                </p>
+                <button>Explore</button>
               </div>
+            </div>
 
-              <div className="boxes">
-                <img src={Main2} alt=""/>
-                <div className="content">
-                  <p>Smart Contract</p>
-                  <h1>AUDITS</h1>
-                  <p>Trust in our expertise to ensure optimal security and performance for your smart contracts within the dynamic web3 landscape.</p>
-                  <button>Explore</button>
-
-                </div>
+            <div className="boxes">
+              <img src={Main3} alt="" />
+              <div className="content">
+                <p>Smart Contract</p>
+                <h1>AUDITS</h1>
+                <p>
+                  Trust in our expertise to ensure optimal security and
+                  performance for your smart contracts within the dynamic web3
+                  landscape.
+                </p>
+                <button>Explore</button>
               </div>
+            </div>
 
-
-              <div className="boxes">
-                <img src={Main3} alt=""/>
-                <div className="content">
-                  <p>Smart Contract</p>
-                  <h1>AUDITS</h1>
-                  <p>Trust in our expertise to ensure optimal security and performance for your smart contracts within the dynamic web3 landscape.</p>
-                  <button>Explore</button>
-
-                </div>
+            <div className="boxes">
+              <img src={Main4} alt="" />
+              <div className="content">
+                <p>Smart Contract</p>
+                <h1>AUDITS</h1>
+                <p>
+                  {" "}
+                  Trust in our expertise to ensure optimal security and
+                  performance for your smart contracts within the dynamic web3
+                  landscape.
+                </p>
+                <button>Explore</button>
               </div>
-
-
-              <div className="boxes">
-                <img src={Main4} alt=""/>
-                <div className="content">
-                  <p>Smart Contract</p>
-                  <h1>AUDITS</h1>
-                  <p>Trust in our expertise to ensure optimal security and performance for your smart contracts within the dynamic web3 landscape.</p>
-                  <button>Explore</button>
-
-                </div>
-              </div>
-
             </div>
           </div>
-        </section>
-      </main>
-   
+        </div>
+      </section>
+    </main>
   );
 }
 
