e6b028078424dde4ff84ef1c652116936bdda65d
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2023-04-16 00:15:07 +0500

    added hover comp

--- src/layouts/About.js
+++ src/layouts/About.js
@@ -7,15 +7,62 @@ import Main2 from "../assets/about/main2.png";
 import Main22 from "../assets/about/main22.png";
 import Main3 from "../assets/about/main3.png";
 import Main4 from "../assets/about/main4.png";
-import Block from '../assets/main-logo.png'
-import {RiShareBoxFill} from 'react-icons/ri'
-import {BsTelegram} from 'react-icons/bs'
+import Block from "../assets/main-logo.png";
+import { RiShareBoxFill } from "react-icons/ri";
+import { BsTelegram } from "react-icons/bs";
+
+const boxVariants = [
+  "scale(1)",
+  "scaleX(-1) scaleY(1)",
+  "scaleX(1) scaleY(-1)",
+  "scaleX(-1) scaleY(-1)",
+];
+
+const CuttedBox = ({ variant = 0 }) => {
+  return (
+    <div className="boxes">
+      <div
+        className={`cutted-box-wrapper `}
+        style={{ transform: boxVariants[variant] }}
+      >
+        <div className="cutted-box-wrapper-box">
+          <img src="images/cutted-box.png" className="cutted-box" alt="" />
+          <img
+            src="images/cutted-box-red.png"
+            className="cutted-box-red"
+            alt=""
+          />
+          <img
+            src="images/cutted-box-gradient.png"
+            className="cutted-box-gradient"
+            alt=""
+          />
+        </div>
+      </div>
+
+      <div className="content">
+        <p>Smart Contract</p>
+        <h1>AUDITS</h1>
+        <p className="con-p">
+          Trust in our expertise to ensure optimal security and performance for
+          your smart contracts within the dynamic web3 landscape.
+        </p>
+        <button className="btn-about fs-20px white weight-4 pointer flex-btn">
+          Explore{" "}
+          <span style={{ color: "#fff" }}>
+            <RiShareBoxFill />
+          </span>{" "}
+        </button>
+      </div>
+    </div>
+  );
+};
 
 function About() {
   return (
     <main>
       <section>
-        <div className="container-wrapper" >
+        <div className="container-wrapper">
           <div className="text-center why-head">
             <h1>What We Offer</h1>
             <p className="">
@@ -27,63 +74,75 @@ function About() {
           </div>
 
           <div className="grid-about">
-            <div className="boxes">
-              {/* <img src={Main1} alt="" /> */}
-              <HoverImage src={Main1} hoverSrc={Main11} />
-              <div className="content">
-                <p>Smart Contract</p>
-                <h1>AUDITS</h1>
-                <p className="con-p">
-                  Trust in our expertise to ensure optimal security and
-                  performance for your smart contracts within the dynamic web3
-                  landscape.
-                </p>
-                <button className='btn-about fs-20px white weight-4 pointer flex-btn'>Explore <span style={{color:'#fff'}}><RiShareBoxFill /></span> </button>
-              </div>
-            </div>
+            <CuttedBox />
 
-            <div className="boxes">
-            <HoverImage src={Main2} hoverSrc={Main22} />
-              {/* <img src={Main2} alt="" /> */}
+            <CuttedBox variant={1} />
+
+            {/* <div className="boxes">
+              <HoverImage src={Main2} hoverSrc={Main22} />
               <div className="content-2">
                 <p>Advanced</p>
                 <h1>Pen Test</h1>
                 <p className="con-p">
-                Utilising advanced techniques to identify and eliminate potential security risks, you can rest assured that your project is secure and optimised.
+                  Utilising advanced techniques to identify and eliminate
+                  potential security risks, you can rest assured that your
+                  project is secure and optimised.
                 </p>
-                <button className='btn-about fs-20px white weight-4 pointer flex-btn'>Explore <span style={{color:'#fff'}}><RiShareBoxFill /></span> </button>
+                <button className="btn-about fs-20px white weight-4 pointer flex-btn">
+                  Explore{" "}
+                  <span style={{ color: "#fff" }}>
+                    <RiShareBoxFill />
+                  </span>{" "}
+                </button>
               </div>
-            </div>
-<div className="center-logo">
-  <img src={Block} alt="" />
-</div>
-            <div className="boxes">
+            </div> */}
+
+            <img src={Block} className="center-logo" alt="" />
+
+            <CuttedBox variant={2} />
+
+            <CuttedBox variant={3} />
+
+            {/* <div className="boxes">
               <img src={Main3} alt="" />
               <div className="content">
                 <p>Risk</p>
                 <h1>Management</h1>
                 <p className="con-p">
-                Ensure industry-standard methodologies to evaluate your project's risk profile and identify and mitigate potential threats to your digital infrastructure.
+                  Ensure industry-standard methodologies to evaluate your
+                  project's risk profile and identify and mitigate potential
+                  threats to your digital infrastructure.
                 </p>
-                <button className='btn-about fs-20px white weight-4 pointer flex-btn'>Contact Us <span style={{color:'#fff'}}><BsTelegram /></span> </button>
+                <button className="btn-about fs-20px white weight-4 pointer flex-btn">
+                  Contact Us{" "}
+                  <span style={{ color: "#fff" }}>
+                    <BsTelegram />
+                  </span>{" "}
+                </button>
               </div>
-            </div>
+            </div> */}
 
-            <div className="boxes">
+            {/* <div className="boxes">
               <img src={Main4} alt="" />
               <div className="content-2">
                 <p>Security Policy</p>
                 <h1>Consultation</h1>
                 <p className="con-p">
                   {" "}
-                  We develop customised security policies and provide guidance on regulatory compliances, risk management, and incident response planning.
+                  We develop customised security policies and provide guidance
+                  on regulatory compliances, risk management, and incident
+                  response planning.
                 </p>
-                <button className='btn-about fs-20px white weight-4 pointer flex-btn'>Contact Us <span style={{color:'#fff'}}><BsTelegram /></span> </button>
+                <button className="btn-about fs-20px white weight-4 pointer flex-btn">
+                  Contact Us{" "}
+                  <span style={{ color: "#fff" }}>
+                    <BsTelegram />
+                  </span>{" "}
+                </button>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
-       
       </section>
     </main>
   );
--- src/layouts/Hero.js
+++ src/layouts/Hero.js
@@ -1,24 +1,20 @@
 import IconCard from "components/IconCard";
 import styles from "scss/layout/Hero.module.scss";
 import { IKImage } from "imagekitio-react";
-import Kyc from '../assets/hero/kyc.png'
-import Audit from '../assets/hero/audit.png'
-import Consult from '../assets/hero/consult.png'
+import Kyc from "../assets/hero/kyc.png";
+import Audit from "../assets/hero/audit.png";
+import Consult from "../assets/hero/consult.png";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css";
 import useMediaQuery from "hooks/useMediaQuery";
-
 
 function Hero() {
   const isBellow1024px = useMediaQuery("(max-width : 64em)");
 
   return (
     <div className={styles.heroWrapper}>
-   
-
       <div className="container-wrapper">
         <div className={styles.hero}>
-          
           <main>
             <h2 className={`fs-50px white weight-3 lh-1 ${styles.preTitle}`}>
               Audit <span className="red">Service</span> For
@@ -28,53 +24,52 @@ function Hero() {
               options={{
                 arrows: false,
                 pagination: false,
-                autoWidth: true,
+                autoWidth: false,
                 gap: 30,
                 autoplay: true,
                 interval: 3000,
                 type: "loop",
-                focus: isBellow1024px ? "center" : "center",
-                
+                // focus: isBellow1024px ? "center" : "center",
               }}
               className={`${styles.splideSlider} splideSlider mb-15px`}
             >
               <SplideSlide>
-                <h1 className="fs-150px white weight-8 lh-1">
+                <h1 className="fs-150px lh-1 white weight-8">
                   DApp<span className="red">.</span>
                 </h1>
               </SplideSlide>
               <SplideSlide>
-                <h1 className="fs-150px white weight-8 lh-1">
+                <h1 className="fs-150px lh-1 white weight-8">
                   DeFi<span className="red">.</span>
                 </h1>
               </SplideSlide>
               <SplideSlide>
-                <h1 className="fs-150px white weight-8 lh-1">
+                <h1 className="fs-150px lh-1 white weight-8">
                   GameFi<span className="red">.</span>
                 </h1>
               </SplideSlide>
               <SplideSlide>
-                <h1 className="fs-150px white weight-8 lh-1">
+                <h1 className="fs-150px lh-1 white weight-8">
                   P2E<span className="red">.</span>
                 </h1>
               </SplideSlide>
               <SplideSlide>
-                <h1 className="fs-150px white weight-8 lh-1">
+                <h1 className="fs-150px lh-1 white weight-8">
                   Contract<span className="red">.</span>
                 </h1>
               </SplideSlide>
               <SplideSlide>
-                <h1 className="fs-150px white weight-8 lh-1">
+                <h1 className="fs-150px lh-1 white weight-8">
                   Token<span className="red">.</span>
                 </h1>
               </SplideSlide>
               <SplideSlide>
-                <h1 className="fs-150px white weight-8 lh-1">
+                <h1 className="fs-150px lh-1 white weight-8">
                   Coin<span className="red">.</span>
                 </h1>
               </SplideSlide>
               <SplideSlide>
-                <h1 className="fs-150px white weight-8 lh-1">
+                <h1 className="fs-150px lh-1 white weight-8">
                   Blockchain<span className="red">.</span>
                 </h1>
               </SplideSlide>
@@ -85,8 +80,7 @@ function Hero() {
               style={{ maxWidth: "22em" }}
             >
               <span className="opacity-0_6">
-              Audit, KYC, Escrow, and Development -<br/> 
-
+                Audit, KYC, Escrow, and Development -<br />
               </span>{" "}
               <span className=" opacity-0_6 ">One stop solution.</span>
             </p>
@@ -99,41 +93,41 @@ function Hero() {
           </main>
 
           <aside>
-        <div className=" bg-grand form-head ">
-          <div className="text-center header-form">
-            <h1 className="">
-            Rapid Audit Request
-            </h1>
-            <p>Complete the form, and we'll get in touch</p>
-          </div>
-          <form className="h-form">
-          <input
-            type="text"
-            className="fs-26px white weight-3"
-            placeholder="Source Code"
-            name="projectname"
-          />
-          <input
-            type="email"
-            className="fs-26px white weight-3"
-            placeholder="Email*"
-            name="email"
-          />
-          <input
-            type="text"
-            className="fs-26px white weight-3"
-            placeholder="Telegram / Discord"
-            name="telegram"
-          />
-         
-          
-<div className='btn-modal'>
-          <button type="submit" className="fs-26px white weight-5 pointer send-btn" >
-            Submit 
-          </button>
-          </div>
-          </form>
-        </div>
+            <div className=" bg-grand form-head ">
+              <div className="text-center header-form">
+                <h1 className="">Rapid Audit Request</h1>
+                <p>Complete the form, and we'll get in touch</p>
+              </div>
+              <form className="h-form">
+                <input
+                  type="text"
+                  className="fs-26px white weight-3"
+                  placeholder="Source Code"
+                  name="projectname"
+                />
+                <input
+                  type="email"
+                  className="fs-26px white weight-3"
+                  placeholder="Email*"
+                  name="email"
+                />
+                <input
+                  type="text"
+                  className="fs-26px white weight-3"
+                  placeholder="Telegram / Discord"
+                  name="telegram"
+                />
+
+                <div className="btn-modal">
+                  <button
+                    type="submit"
+                    className="fs-26px white weight-5 pointer send-btn"
+                  >
+                    Submit
+                  </button>
+                </div>
+              </form>
+            </div>
           </aside>
         </div>
       </div>
