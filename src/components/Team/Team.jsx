4239342ac24ca73972a50ae5832bbb71c46bbdc1
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-04-15 05:43:36 +0530

    new

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -15,87 +15,90 @@ SwiperCore.use([Navigation, Pagination, EffectCoverflow]);
 
 export default function SwiperCoverflow() {
   return (
-    <div className="container-wrapper">
+    <div className="conatiner-wrapper">
       <Swiper
         navigation
         pagination={{ clickable: true }}
         effect="coverflow"
         coverflowEffect={{
-          rotate: 50,
+          rotate: -21,
           stretch: 0,
           depth: 100,
           modifier: 1,
           slideShadows: true,
         }}
-        slidesPerView={3}
+        slidesPerView={2}
         centeredSlides
-        style={{ height: "500px" }}
+        style={{ height: "600px" }}
       >
         <SwiperSlide>
           <div className="team-box">
-            <img src={CotImg} alt="" />
-            <div>
+            <img src={CotImg} alt="" className="colon" />
+            <div className="team-details">
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              BlockAudit exceeded our expectations in providing exceptional Smart Contract Auditing and Risk Management services. 
+
+
               </p>
+
+              <p>Their experienced team, in-depth knowledge, and personalized approach made working with them an absolute pleasure. We highly recommend BlockAudit for top-tier web3 project security and compliance services.</p>
             </div>
-            <div>
+            <div className="avatar-flex">
               <div>
                 <img src={AvtImg} alt="" />
               </div>
               <div>
-                <p>John Doe</p>
+                <p className="user-name">John Doe</p>
+                <p className="desi">Web3 Warriors</p>
+                <p className="user-social">@Web3_Warriors</p>
               </div>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="team-box">
-            <img src={CotImg} alt="" />
-            <div>
+            <img src={CotImg} alt="" className="colon"  />
+           <div className="team-details">
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              BlockAudit exceeded our expectations in providing exceptional Smart Contract Auditing and Risk Management services. 
+
+
               </p>
+              
+              <p>Their experienced team, in-depth knowledge, and personalized approach made working with them an absolute pleasure. We highly recommend BlockAudit for top-tier web3 project security and compliance services.</p>
             </div>
-            <div>
+            <div className="avatar-flex">
               <div>
                 <img src={AvtImg} alt="" />
               </div>
               <div>
-                <p>John Doe</p>
+              <p className="user-name">John Doe</p>
+                <p className="desi">Web3 Warriors</p>
+                <p className="user-social">@Web3_Warriors</p>
               </div>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="team-box">
-            <img src={CotImg} alt="" />
-            <div>
+            <img src={CotImg} alt="" className="colon"  />
+            <div className="team-details">
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              BlockAudit exceeded our expectations in providing exceptional Smart Contract Auditing and Risk Management services. 
+
+
               </p>
+              
+              <p>Their experienced team, in-depth knowledge, and personalized approach made working with them an absolute pleasure. We highly recommend BlockAudit for top-tier web3 project security and compliance services.</p>
             </div>
-            <div>
+            <div className="avatar-flex">
               <div>
                 <img src={AvtImg} alt="" />
               </div>
               <div>
-                <p>John Doe</p>
+              <p className="user-name">John Doe</p>
+                <p className="desi">Web3 Warriors</p>
+                <p className="user-social">@Web3_Warriors</p>
               </div>
             </div>
           </div>
@@ -103,23 +106,24 @@ export default function SwiperCoverflow() {
 
         <SwiperSlide>
           <div className="team-box">
-            <img src={CotImg} alt="" />
-            <div>
+            <img src={CotImg} alt="" className="colon"  />
+            <div className="team-details">
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              BlockAudit exceeded our expectations in providing exceptional Smart Contract Auditing and Risk Management services. 
+
+
               </p>
+              
+              <p>Their experienced team, in-depth knowledge, and personalized approach made working with them an absolute pleasure. We highly recommend BlockAudit for top-tier web3 project security and compliance services.</p>
             </div>
-            <div>
+            <div className="avatar-flex">
               <div>
                 <img src={AvtImg} alt="" />
               </div>
               <div>
-                <p>John Doe</p>
+              <p className="user-name">John Doe</p>
+                <p className="desi">Web3 Warriors</p>
+                <p className="user-social">@Web3_Warriors</p>
               </div>
             </div>
           </div>
