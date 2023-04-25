e1fa882fb1ee92c6490b199a9dc270daf5c13e19
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-04-26 02:03:18 +0530

    new

--- src/components/Faq/Data.js
+++ src/components/Faq/Data.js
@@ -8,20 +8,30 @@ export const Data= [
     {
         id: 2,
         imgs:'https://ik.imagekit.io/l4k8brcjb/BlockAudit/Faq/Cost.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1660386796706',
-        question: 'How much does BlockAudit charges to audit one smart contract?',
+        question: 'Can you provide examples of web3 projects that you have audited in the past?',
         answer:'Our audit starts from 0 $ (POS). The cost of auditing depends upon various factors such as complexity, functions, length, Blockchain (EVM or Non-EVM) of smart contract.'
     },
     {
         id: 3,
         imgs:'https://ik.imagekit.io/l4k8brcjb/BlockAudit/Faq/Included.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1660386797480',
-        question: 'What all will be included in your comprehensive Audit Report?',
+        question: 'What methodologies do you use for auditing smart contracts?  ',
         answer:'We will provide you with Critical, major and minor bugs along with changes for gas optimization. Recommendation and suggestions shall also be provided to refractor so that smart contract performs as intended.'
     },
     {
         id: 4,
         imgs:'https://ik.imagekit.io/l4k8brcjb/BlockAudit/Faq/Gas.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1660386796766',
-        question: 'How the audit help in gas optimization?',
+        question: 'What are the potential security risks in my smart contract, and how will you identify and mitigate them?',
         answer:'Yes. We will suggest required changes to optimize the gas fee so that your community can have better experience'
     },
+
+    {
+        id: 5,
+        imgs:'https://ik.imagekit.io/l4k8brcjb/BlockAudit/Faq/Gas.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1660386796766',
+        question: 'How do you stay up-to-date with emerging web3 technologies and trends?',
+        answer:'Yes. We will suggest required changes to optimize the gas fee so that your community can have better experience'
+    },
+
+    
+
 
 ]
--- src/components/Faq/Fdata.js
+++ src/components/Faq/Fdata.js
@@ -2,13 +2,13 @@ export const Fdata= [
     {
         id: 1,
         imgs:'https://ik.imagekit.io/l4k8brcjb/BlockAudit/Faq/Support.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1660386798395',
-        question: 'Will your team support in  recommendations of Audit Report?',
+        question: 'How long does the audit process typically take?',
         answer:'Yes. After initial report a time frame shall be provided in which our security expert will be coordinating with your team to refractor the smart contract as per the Audit Report.'
     },
     {
         id: 2,
         imgs:'https://ik.imagekit.io/l4k8brcjb/BlockAudit/Faq/Process.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1660386797556',
-        question: 'What is the process of Auditing at BlockAudit?',
+        question: 'What is the cost of the audit, and how is it calculated?',
         answer:'Requirement gathering -> Automated Testing -> Manual Review -> Manual Testing Initial Report -> Final Report with Stamp'
     },
     {
@@ -20,7 +20,13 @@ export const Fdata= [
     {
         id: 4,
         imgs:'https://ik.imagekit.io/l4k8brcjb/BlockAudit/Faq/Extra_Cost.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1660386796469',
-        question: 'Is there any extra cost for consultation?',
+        question: 'What is your policy on fixing any security issues found during the audit, and will you provide support during the process of fixing any issues?',
+        answer:'We provide our clients first consultation with security experts free of cost. Kindly contact for the same.'
+    },
+    {
+        id: 5,
+        imgs:'https://ik.imagekit.io/l4k8brcjb/BlockAudit/Faq/Extra_Cost.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1660386796469',
+        question: 'What types of identity documents do you accept for KYC, and how do you verify them?',
         answer:'We provide our clients first consultation with security experts free of cost. Kindly contact for the same.'
     },
 
