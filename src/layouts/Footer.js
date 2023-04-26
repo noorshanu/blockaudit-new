f916410cda5cce36f58ae348876ad8d2c9f01f3c
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-04-26 20:54:37 +0530

    Update Footer.js

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -25,7 +25,7 @@ function Footer() {
             <h2>Social Media</h2>
             <div>
             <p> <a href="https://t.me/BlockAuditReport" target="_blank" rel="noreferrer" className="pointer"><span><BsTwitter/> </span> Telegram</a>  </p> 
-              <p><a href="https://www.linkedin.com/company/blockaudit/" target="_blank" rel="noreferrer" > <span><BsLinkedin/> </span> LinkedIn</a></p>
+              <p><a href="https://www.linkedin.com/company/block-audit-report" target="_blank" rel="noreferrer" > <span><BsLinkedin/> </span> LinkedIn</a></p>
               <p><a href="https://twitter.com/BlockAudit" target="_blank" rel="noreferrer" ><span><BsTelegram/> </span> Twitter</a> </p>
               <p><a href="https://github.com/Block-Audit-Report" target="_blank" rel="noreferrer" ><span><BsGithub/> </span> Github</a> </p>
               <p><a href="https://blockauditreport.medium.com/" target="_blank" rel="noreferrer"><span><BsMedium/> </span>Medium</a> </p>
@@ -89,7 +89,7 @@ function Footer() {
 
             <div className="mob-social">
             <p> <a href="https://t.me/BlockAuditReport" target="_blank" rel="noreferrer" className="pointer"><span> <BsTelegram/></span> </a>  </p> 
-              <p><a href="https://www.linkedin.com/company/blockaudit/" target="_blank" rel="noreferrer" > <span><BsLinkedin/> </span> </a></p>
+              <p><a href="https://www.linkedin.com/company/block-audit-report" target="_blank" rel="noreferrer" > <span><BsLinkedin/> </span> </a></p>
               <p><a href="https://twitter.com/BlockAudit" target="_blank" rel="noreferrer" ><span><BsTwitter/> </span> </a> </p>
               <p><a href="https://github.com/Block-Audit-Report" target="_blank" rel="noreferrer" ><span><BsGithub/> </span> </a> </p>
               <p><a href="https://blockauditreport.medium.com/" target="_blank" rel="noreferrer"><span><BsMedium/> </span></a> </p>
