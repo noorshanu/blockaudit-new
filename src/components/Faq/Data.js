b05ec80f25ba0055839545661e20aa94719c06e6
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-05-08 22:10:08 +0530

    new

--- src/components/Faq/Data.js
+++ src/components/Faq/Data.js
@@ -1,32 +1,32 @@
 export const Data= [
     {
         id: 1,
-        imgs:'https://ik.imagekit.io/l4k8brcjb/BlockAudit/Faq/Requiremnet.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1660386797985',
+        imgs:'https://ik.imagekit.io/l4k8brcjb/BlockAudit/Faq/Experience_Icon_FAQ1.svg',
         question: 'What is your experience in auditing smart contracts for web3 projects?',
         answer:'Our team uses manual and automated testing techniques to identify vulnerabilities in the smart contracts code and provide clear and actionable reports to clients. We prioritize any identified issues based on their severity and review the smart contracts design and architecture to ensure it meets project requirements. We are committed to staying up to date with the latest security best practices and emerging threats to provide high-quality auditing services to clients in the web3 space.'
     },
     {
         id: 2,
-        imgs:'https://ik.imagekit.io/l4k8brcjb/BlockAudit/Faq/Cost.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1660386796706',
+        imgs:'https://ik.imagekit.io/l4k8brcjb/BlockAudit/Faq/Example_Icon_FAQ2.svg',
         question: 'Can you provide examples of web3 projects that you have audited in the past?',
         answer:'We respect the confidentiality and privacy of our clients, and therefore we cannot disclose specific details of our past audits without their permission. However, we have audited a wide range of web3 projects across various industries and platforms, including decentralized finance (DeFi), non-fungible tokens (NFTs), gaming, supply chain management, and more.'
     },
     {
         id: 3,
-        imgs:'https://ik.imagekit.io/l4k8brcjb/BlockAudit/Faq/Included.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1660386797480',
+        imgs:'https://ik.imagekit.io/l4k8brcjb/BlockAudit/Faq/Methodology_FAQ3.svg',
         question: 'What methodologies do you use for auditing smart contracts?  ',
         answer:'We use a combination of manual and automated testing methodologies when auditing smart contracts. Our team conducts a thorough review of the smart contract code and design, as well as functional testing under various scenarios to identify any vulnerabilities or flaws. We prioritize any issues based on their severity and provide actionable guidance for developers to address the vulnerabilities.'
     },
     {
         id: 4,
-        imgs:'https://ik.imagekit.io/l4k8brcjb/BlockAudit/Faq/security.svg?updatedAt=1683529546458',
+        imgs:'https://ik.imagekit.io/l4k8brcjb/BlockAudit/Faq/Risk_FAQ4.svg?updatedAt=1683563390915',
         question: 'What are the potential security risks in my smart contract, and how will you identify and mitigate them?',
         answer:'We are well-versed in identifying and mitigating potential security risks. Common risks include logic errors, reentrancy attacks, and other vulnerabilities. We use a variety of tools and techniques to analyze the code, test the contract functionality, and review the design and architecture to ensure that it meets the project requirements and is secure. '
     },
 
     {
         id: 5,
-        imgs:'https://ik.imagekit.io/l4k8brcjb/BlockAudit/Faq/website-reload.svg?updatedAt=1683529893340',
+        imgs:'https://ik.imagekit.io/l4k8brcjb/BlockAudit/Faq/Up-to-dat_FAQ5.svg?updatedAt=1683563390809',
         question: 'How do you stay up-to-date with emerging web3 technologies and trends?',
         answer:'We stay up-to-date with emerging web3 technologies and trends through various means. This includes attending industry events, participating in relevant online communities and forums, and engaging with our clients to understand their specific needs and challenges. Additionally, we prioritize ongoing education and training for our team to ensure that we are equipped with the latest knowledge.'
     },
--- src/components/Faq/Fdata.js
+++ src/components/Faq/Fdata.js
@@ -1,31 +1,31 @@
 export const Fdata= [
     {
         id: 1,
-        imgs:'https://ik.imagekit.io/l4k8brcjb/BlockAudit/Faq/time.svg',
+        imgs:'https://ik.imagekit.io/l4k8brcjb/BlockAudit/Faq/Time_FAQ6.svg?updatedAt=1683563390798',
         question: 'How long does the audit process typically take?',
         answer:'The length of time required to complete a smart contract audit can vary based on factors such as the complexity of the contract and the scope of the project. Typically, an audit can take several days to several weeks, and clear communication with the project team can help to ensure an efficient and effective audit process. An experienced audit firm should be able to provide an estimated timeline based on the project needs.'
     },
     {
         id: 2,
-        imgs:'https://ik.imagekit.io/l4k8brcjb/BlockAudit/Faq/cost-minimize.svg?updatedAt=1683526124926',
+        imgs:'https://ik.imagekit.io/l4k8brcjb/BlockAudit/Faq/Money_FAQ7.svg?updatedAt=1683563390877',
         question: 'What is the cost of the audit, and how is it calculated?',
         answer:'The cost of a smart contract audit can vary depending on the complexity of the contract, the scope of the audit, and the experience level of the auditing firm. BlockAudit will provide a quote based on the estimated time required to complete the audit, taking into account factors such as the size of the codebase, the level of manual review required, and any additional testing or verification needs.'
     },
     {
         id: 3,
-        imgs:'https://ik.imagekit.io/l4k8brcjb/BlockAudit/Faq/Tool.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1660386798966',
+        imgs:'https://ik.imagekit.io/l4k8brcjb/BlockAudit/Faq/Automated_FAQ8.svg?updatedAt=1683563390865',
         question: 'What all automated tools will be used?',
         answer:'We use a variety of automated tools to assist in our auditing process. These tools include static analysis tools to identify potential code issues, automated testing frameworks to test the smart contract  functionality, and security analysis tools to identify vulnerabilities and attack vectors. We also use our own tool (BlockRadar) to better understand the smart contract behaviour and interactions with the blockchain network.'
     },
     {
         id: 4,
-        imgs:'https://ik.imagekit.io/l4k8brcjb/BlockAudit/Faq/policy.svg?updatedAt=1683528097242',
+        imgs:'https://ik.imagekit.io/l4k8brcjb/BlockAudit/Faq/Support_9.svg?updatedAt=1683563390915',
         question: 'What is your policy on fixing any security issues found during the audit, and will you provide support during the process of fixing any issues?',
         answer:'Our policy is to prioritize security issues found during audits and work with clients to remediate them. We provide clear and actionable guidance on how to fix any vulnerabilities, and offer support throughout the process to ensure that issues are addressed in a timely and effective manner. Our goal is to help clients create secure and reliable smart contracts'
     },
     {
         id: 5,
-        imgs:'https://ik.imagekit.io/l4k8brcjb/BlockAudit/Faq/identity-card.svg',
+        imgs:'https://ik.imagekit.io/l4k8brcjb/BlockAudit/Faq/Documentation_FAQ10.svg?updatedAt=1683563390806',
         question: 'What types of identity documents do you accept for KYC, and how do you verify them?',
         answer:'We accept government-issued identification documents such as passports, driver licenses, and national identification cards for KYC. We use social media verification to verify the authenticity of these documents and to confirm the identity of the person submitting them. Our verification process includes checks for name, address, and date of birth, as well as other security measures to prevent fraud and identity theft.'
     },
